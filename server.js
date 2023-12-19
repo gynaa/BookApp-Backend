4022dc21e2f1ae6bfcf829a0bb01f06bf77d31b1
gynaa <116661372+gynaa@users.noreply.github.com>
2023-12-19 12:02:27 +0200

    make it work on .com

--- server.js
+++ server.js
@@ -3,12 +3,14 @@
 require('./config/db');
 
 const app = require('express')();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const UserRouter = require('./api/user');
 
 const bodyParser = require('express').json;
 app.use(bodyParser());
+const cors = require("cors");
+app.use(cors());
 
 app.use('/user', UserRouter)
 
