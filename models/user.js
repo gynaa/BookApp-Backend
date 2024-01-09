12601e9db6eec49ff14b1062a2c8e266c5b4a50f
gynaa <116661372+gynaa@users.noreply.github.com>
2024-01-09 14:27:16 +0200

    Your commit message here

--- models/user.js
+++ models/user.js
@@ -5,7 +5,9 @@ const UserSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    location: { type: String, required: true }
+    location: { type: String, required: true },
+    bio: { type: String, required: false },            // Optional field for the user's bio
+    favoriteBook: { type: String, required: false } 
 });
 
 const User = mongoose.model('User', UserSchema);
