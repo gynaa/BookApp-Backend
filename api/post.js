a262201fc9fc540a1345660c58cf1d718bb37962
gynaa <116661372+gynaa@users.noreply.github.com>
2024-02-18 12:20:07 +0200

    'post'

---
+++ api/post.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const multer =require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const Storage = multer.memoryStorage({
+    destination: "uploads",
+    filename: (req, file, cb) =>{
+        cb(null, file.originalname);
+    },});
+
+const upload = multer({
+    storage:Storage
+}).single('testImage')
+
+//mongodb user model
+const Post = require('./../models/post')
+
+//Password handler
+const bcrypt = require('bcrypt');
+
+
+router.post('/postupload', async (req, res) => {
+    let{email, postbio, posttitle, postauthor, postImage } = req.body;
+
+
+    try {
+        const newPost = new Post({
+            email,
+            postbio,
+            posttitle, 
+            postauthor, 
+            postImage
+        });
+        
+        newPost.save().then(result => {
+            res.json({
+                status: "SUCCESS",
+                message: "Posting successful",
+                data: result,
+            })
+        })
+        .catch(err => {
+            res.json({
+                status: "FAILED",
+                message: "Error while posting post!"
+            })
+        
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+module.exports = router;
+
---
+++ models/post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ImageSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    image:{
+        data:Buffer,
+        contentType: String
+    }
+})
+
+const PostSchema = new Schema({
+    email: { type: String, required: true },
+    postbio: { type: String, required: false },
+    posttitle: { type: String, required: false },
+    postauthor: { type: String, required: false },
+    postImage: ImageSchema,
+});
+
+
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
